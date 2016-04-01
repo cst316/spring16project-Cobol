02e0a594f4f21a14b84fa201797d4b457f84bfb4
Julian Douglas <jbdougl2@asu.edu>
2016-04-01 11:10:29 -0700

    Added ability to create task based off of template
    Added ability to create task based off of template

--- src/net/sf/memoranda/TaskTemplateImpl.java
+++ src/net/sf/memoranda/TaskTemplateImpl.java
@@ -1,5 +1,54 @@
 package net.sf.memoranda;
 
-public class TaskTemplateImpl implements TaskTemplate {
+import java.util.Date;
+import java.util.*;
+import net.sf.memoranda.date.CalendarDate;
 
+public class TaskTemplateImpl implements TaskTemplate 
+{
+	//Fields to populate 
+		CalendarDate calendarDateStart;
+		
+		CalendarDate calendarDateEnd;
+		
+		int progressToSet;
+		
+		int priorityToSet;
+		
+		String textToSet;
+		
+		long effortToSet;
+		
+		String descriptionToSet;
+	
+	public TaskTemplateImpl(int autoFillNum)
+	{
+		if(autoFillNum == 0)
+		{
+			Calendar calendar = Calendar.getInstance();
+
+			Date BeginDate = calendar.getTime();
+			calendarDateStart = new CalendarDate(BeginDate);
+			
+			calendar.add(Calendar.DAY_OF_YEAR, 7);
+			Date EndDate = calendar.getTime();
+			calendarDateEnd = new CalendarDate(EndDate);
+			
+			progressToSet = 0;
+			
+			priorityToSet = 0;
+			
+			textToSet = "This is Test Template Task";
+			
+			effortToSet = 0;
+			
+			descriptionToSet = "This is Test Template";
+		}
+	}
+	
+	public Task createTask()
+	{
+		Task taskToPopulate = CurrentProject.getTaskList().createTask(calendarDateStart, calendarDateEnd, textToSet, progressToSet, priorityToSet, textToSet, null);
+		return taskToPopulate;
+	}
 }
--- src/net/sf/memoranda/ui/TaskTemplateDialogue.java
+++ src/net/sf/memoranda/ui/TaskTemplateDialogue.java
@@ -15,6 +15,8 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import net.sf.memoranda.*;
+import net.sf.memoranda.util.CurrentStorage;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -47,7 +49,8 @@ public class TaskTemplateDialogue extends JDialog
 {
 	JPanel mainPanel = new JPanel(new BorderLayout());
 	JButton standardTemplate_B = new JButton();
-	JButton newTemplate_B = new JButton();
+	JButton cancelButton = new JButton();
+	TaskTemplate CurrentTaskTemplate;
 	
 	Border border1;
 	
@@ -60,36 +63,50 @@ public class TaskTemplateDialogue extends JDialog
 
 	void ttInit()
 	{
-		this.setResizable(false);
-		this.setSize(new Dimension(1200, 800));
+		this.setResizable(true);
+		this.setSize(new Dimension(1400, 800));
         border1 = BorderFactory.createEmptyBorder(5, 5, 5, 5);
         
         //mainPanel.setBorder(border1);
         
-        standardTemplate_B.setText("Standard Template");
-        standardTemplate_B.setMinimumSize(new Dimension(100, 26));
+        standardTemplate_B.setText("Test Template");
+        //standardTemplate_B.setMinimumSize(new Dimension(100, 26));
         standardTemplate_B.setVisible(true);
         standardTemplate_B.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(ActionEvent e) {
-            	dispose();
+            	//dispose();
             	standardTemplate_B_Action(e);
             }
         });
         
+        cancelButton.setText("Cancel");
+        //cancelButton.setMinimumSize(new Dimension(50, 26));
+        cancelButton.setVisible(true);
+        cancelButton.addActionListener(new java.awt.event.ActionListener(){
+        	public void actionPerformed(ActionEvent e)
+        	{
+        		cancelButtonAction(e);
+        	}
+        });
+        
         this.add(mainPanel, null);
         mainPanel.setBorder(border1);
-        mainPanel.add(standardTemplate_B, null);
+        mainPanel.add(standardTemplate_B);
+        mainPanel.add(cancelButton);
+        standardTemplate_B.setBounds(10,10,40,40);
+        cancelButton.setBounds(10,10,40,40);
 	}
 	
 	void standardTemplate_B_Action (ActionEvent e)
 	{
-		TaskDialog dlg = new TaskDialog(App.getFrame(), Local.getString("New task"));
-		
-		Dimension frmSize = App.getFrame().getSize();
-        Point loc = App.getFrame().getLocation();
-		dlg.startDate.getModel().setValue(CurrentDate.get().getDate());
-        dlg.endDate.getModel().setValue(CurrentDate.get().getDate());
-        dlg.setLocation((frmSize.width - dlg.getSize().width) / 2 + loc.x, (frmSize.height - dlg.getSize().height) / 2 + loc.y);
-        dlg.setVisible(true);
+		CurrentTaskTemplate = new TaskTemplateImpl(0);
+		CurrentTaskTemplate.createTask();
+		CurrentStorage.get().storeTaskList(CurrentProject.getTaskList(), CurrentProject.get());
+		dispose();
+	}
+	
+	void cancelButtonAction(ActionEvent e)
+	{
+		dispose();
 	}
 }
